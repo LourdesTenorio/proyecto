bd33776abd56e256fe6253b52b8b495753a864db
lourdestenorio <lourdes.99101@gmail.com>
2023-11-18 20:21:18 -0500

    cambios

--- ms_config_server/src/test/java/pe/cibertec/proyecto/configserver/ms_config_server/MsConfigServerApplicationTests.java
+++ ms_config_server/src/test/java/pe/cibertec/proyecto/configserver/ms_config_server/MsConfigServerApplicationTests.java
@@ -2,7 +2,8 @@ package pe.cibertec.proyecto.configserver.ms_config_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.cloud.config.server.EnableConfigServer;
+@EnableConfigServer
 @SpringBootTest
 class MsConfigServerApplicationTests {
 
