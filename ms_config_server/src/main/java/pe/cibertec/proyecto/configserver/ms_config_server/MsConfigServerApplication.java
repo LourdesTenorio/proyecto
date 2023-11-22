3b5015d59c5e05c9f6f5b248cf50a52135951da4
lourdestenorio <lourdes.99101@gmail.com>
2023-11-21 23:59:56 -0500

    segundo cambio

--- ms_config_server/src/main/java/pe/cibertec/proyecto/configserver/ms_config_server/MsConfigServerApplication.java
+++ ms_config_server/src/main/java/pe/cibertec/proyecto/configserver/ms_config_server/MsConfigServerApplication.java
@@ -2,7 +2,8 @@ package pe.cibertec.proyecto.configserver.ms_config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.config.server.EnableConfigServer;
+@EnableConfigServer
 @SpringBootApplication
 public class MsConfigServerApplication {
 
